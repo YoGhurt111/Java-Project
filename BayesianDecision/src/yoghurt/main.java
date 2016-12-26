4772746b1734d92c2e23e3d4991b8041e1bed395
YoGhurt <623770278@qq.com>
2016-12-26 15:22:10 +0800

    基于最小错误率单特征Bayes决策

--- BayesianDecision/src/yoghurt/main.java
+++ BayesianDecision/src/yoghurt/main.java
@@ -5,13 +5,13 @@ public class main {
     public static void main(String[] args){
         Bayes bayes = new Bayes();
         //训练样本
-        bayes.setHeightOfBoy("d:/BayesianDecision/boy.txt");
-        bayes.setHeightOfGril("d:/BayesianDecision/girl.txt");
+        bayes.setHeightOfBoy("boy.txt");
+        bayes.setHeightOfGril("girl.txt");
         bayes.setData();
         //分类测试
-        bayes.setPriorP1(0.75);
-        bayes.setHeightOfBoy("d:/BayesianDecision/boynew.txt");
-        bayes.setHeightOfGril("d:/BayesianDecision/girlnew.txt");
+        bayes.setPriorP1(0.5);
+        bayes.setHeightOfBoy("boynew.txt");
+        bayes.setHeightOfGril("girlnew.txt");
         bayes.testing();
     }
 }
